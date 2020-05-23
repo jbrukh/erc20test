9ad81d8da16d1ef0bff30cf970ed3bc477f592b5
Jake Brukhman <jake@coinfund.io>
2020-05-23 15:55:30 -0400

    Added rinkeby

--- buidler.config.ts
+++ buidler.config.ts
@@ -4,6 +4,9 @@ usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("@nomiclabs/buidler-truffle5");
 usePlugin("buidler-typechain");
 
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
 const config: BuidlerConfig = {
   solc: {
     version: "0.6.2"
@@ -11,7 +14,13 @@ const config: BuidlerConfig = {
   typechain: {
     outDir: "typechain",
     target: "ethers"
-  }
+  },
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`${RINKEBY_PRIVATE_KEY}`]
+    }
+  },
 };
 
 export default config;
